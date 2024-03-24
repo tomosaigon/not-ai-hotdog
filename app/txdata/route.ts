a2a61df1958eb4cea7de88c3ea66083c07067dda
tomo <tomo@crowdeo.com>
2024-03-24 14:10:08 +0700

    feat: mint NOT

--- app/txdata/route.ts
+++ app/txdata/route.ts
@@ -3,51 +3,51 @@ import { TransactionTargetResponse } from "frames.js";
 import { getFrameMessage } from "frames.js/next/server";
 import { NextRequest, NextResponse } from "next/server";
 import {
-  Abi,
-  createPublicClient,
+  // Abi,
+  // createPublicClient,
+  // createWalletClient,
+  // custom,
   encodeFunctionData,
-  getContract,
-  http,
+  // getContract,
+  // http,
 } from "viem";
-import { sepolia } from "viem/chains";
+// import { sepolia } from "viem/chains";
 // import { storageRegistryABI } from "./contracts/storage-registry";
-import deployedContracts from "./contracts/gho";
+// import deployedContracts from "./contracts/gho";
+import abi from "./contracts/erc721";
+const address = '0xf85d352c3c62cf0823aa58233509c7332e0bafd2'; // contract NotDog is ERC721, ERC721Enumerable, Ownable
 
 export async function POST(
   req: NextRequest
 ): Promise<NextResponse<TransactionTargetResponse>> {
   const json = await req.json();
+  // const untrustedDataAddress = json.untrustedData.address;
+  // console.log('json', json)
 
   const frameMessage = await getFrameMessage(json);
+  // console.log('frameMessage', frameMessage)
 
   if (!frameMessage) {
     throw new Error("No frame message");
   }
+  const connectedAddress = frameMessage.connectedAddress as `0x${string}` | null;
 
-  // Get current storage price
-  // const units = 1n;
-
-  // const calldata = encodeFunctionData({
-  //   abi: storageRegistryABI,
-  //   functionName: "rent",
-  //   args: [BigInt(frameMessage.requesterFid), units],
-  // });
   const calldata = encodeFunctionData({
-    abi: deployedContracts[11155111].GhoToken.abi,
-    functionName: "mint",
-    args: ['0xE3c382A8B72643CC3756D532e967Eb44e885c619', BigInt(100)],
+    abi,
+    functionName: "safeMint",
+    args: [connectedAddress ? connectedAddress : `0x${"0".repeat(20)}`], // XXX
   });
 
-  const publicClient = createPublicClient({
-    chain: sepolia,
-    transport: http(),
-  });
+  // const publicClient = createPublicClient({
+  //   chain: sepolia,
+  //   transport: http(),
+  // });
 
-  const gho = getContract({
-    address: deployedContracts[11155111].GhoToken.address,
-    abi: deployedContracts[11155111].GhoToken.abi,
-    client: publicClient,
-  });
+  // const notdog = getContract({
+  //   address,0xf85d352c3c62cf0823aa58233509c7332e0bafd2
+  //   abi,
+  //   client: publicClient,
+  // });
 
   // const balanceOf = await gho.read.balanceOf(['0xE3c382A8B72643CC3756D532e967Eb44e885c619']);
 
@@ -56,8 +56,8 @@ export async function POST(
     chainId: "eip155:11155111", // OP Mainnet 10
     method: "eth_sendTransaction",
     params: {
-      abi: deployedContracts[11155111].GhoToken.abi as Abi,
-      to: deployedContracts[11155111].GhoToken.address,
+      abi,
+      to: address,
       data: calldata,
       // value: balanceOf.toString(),
       value: '0',
